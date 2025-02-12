b3d2ea0f8ea5bf706f7ea21fd3ec1e691520d232
mlkamri <mlkalamri0@gmail.com>
2025-02-13 02:27:23 +0300

    Debugging

--- src/screens/UserPlanScreen.js
+++ src/screens/UserPlanScreen.js
@@ -253,6 +253,7 @@ export default function UserPlan() {
         </View>
       </View>
 
+      {/* Body */}
       <ScrollView
         contentContainerStyle={styles.container}
         showsVerticalScrollIndicator={false}
