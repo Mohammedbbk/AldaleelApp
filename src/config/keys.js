139299ad7ba0489efb63f04a3a4139d1f2630e34
Mohammedbbk <mohabashuni@gmail.com>
2025-05-06 16:57:33 +0300

    removed one line

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,4 +1,3 @@
 // Configuration file for API keys and sensitive data
-export const OPENWEATHERMAP_API_KEY = process.env.OPENWEATHERMAP_API_KEY || '3f141daba7c7fb2740e27f544145b76f';
 
-// Add other API keys and configuration values here
+export const OPENWEATHERMAP_API_KEY = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
